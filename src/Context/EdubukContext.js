6ab44d4cd855ec388d78914eaff6237d94b3f9e7
ajeetram <86039147+ajeetram@users.noreply.github.com>
2024-09-29 16:53:34 +0530

    basic ui completed

---
+++ src/Context/EdubukContext.js
@@ -0,0 +1,38 @@
+import React,{createContext, useState,useEffect} from 'react'
+import { connectWallet,connectingWithContract } from '../Utils/apiFeature'
+
+export const EdubukContexts = createContext();
+export const EdubukProvider = ({children}) => {
+    const [account, setAccount] = useState("");
+    const [studentName, setStudentName] = useState("");
+    const [openSidebar, setOpenSidebar] = useState(false);
+
+    const fetchData = async()=>{
+        try {
+            const connectedAcc = await connectWallet();
+            setAccount(connectedAcc);
+            console.log("Connected Account : ",connectedAcc);
+        } catch (error) {
+            console.log("Error in fetchdata : ",error);
+        }
+    }
+    
+    useEffect(()=>{
+        fetchData();
+        
+    },[])
+  return (
+    <EdubukContexts.Provider
+    value = {{connectWallet,
+    connectingWithContract,
+    account,
+    studentName,
+    setStudentName,
+    openSidebar,
+    setOpenSidebar}}
+    >
+        {children}
+    </EdubukContexts.Provider>
+  )
+}
+
---
+++ src/Context/constant.js
@@ -0,0 +1,4 @@
+import ABI from './edubuksc.json'
+//XDC test network by aproova sir
+export const EdubukConAdd = "0xEC0B305e6B4d3236CfA7d645631D04b459dd197b";
+export const EdubukConABI = ABI.abi;
